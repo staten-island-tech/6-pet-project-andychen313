d430706956e55af0f7bbbbd755284b3fe5bf8e83
andychen313 <andyc313@nycstudents.net>
2025-10-30 09:25:38 -0400

    Create pet.py

---
+++ pet.py
@@ -0,0 +1,11 @@
+class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+    def buy(self, item):
+        self.inventory.append(item)
+        print(f"{self.name} purchased {item} and has {self.inventory}")
+Nathan = Hero("Nathan", 0, ["Pencil"])
+print(Nathan.__dict__)
+Nathan.buy("Xi Yang")
