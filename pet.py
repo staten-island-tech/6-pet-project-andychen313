23e81e7f906e107a240528014006ab843cd4369f
andychen313 <andyc313@nycstudents.net>
2025-10-31 09:26:12 -0400

    commit

--- pet.py
+++ pet.py
@@ -1,4 +1,4 @@
-class Hero:
+""" class Hero:
     def __init__(self, name, money, inventory):
         self.name = name
         self.money = money
@@ -8,4 +8,32 @@ class Hero:
         print(f"{self.name} purchased {item} and has {self.inventory}")
 Nathan = Hero("Nathan", 0, ["Pencil"])
 print(Nathan.__dict__)
-Nathan.buy("Xi Yang")
+Nathan.buy("Xi Yang") """
+
+""" class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+Jillian = Hero("Jillian", 150, ["Potion"])
+print(Jillian.__dict__)
+
+Jillian.buy({"title": "Sword", "atk": 34})
+print(Jillian.__dict__) """
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance +=amount
+
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}")
+Andy = BankAccount("Andy", 150)
+print(Andy.__dict__)
