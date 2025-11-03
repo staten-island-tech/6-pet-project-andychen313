cbbf00415ef575fddf546c7e10cf73b0038b003a
andychen313 <andyc313@nycstudents.net>
2025-11-03 09:26:18 -0500

    Update pet.py

--- pet.py
+++ pet.py
@@ -25,7 +25,7 @@ print(Jillian.__dict__)
 Jillian.buy({"title": "Sword", "atk": 34})
 print(Jillian.__dict__) """
 
-class BankAccount:
+""" class BankAccount:
     def __init__(self, owner, balance):
         self.owner = owner
         self.__balance = balance
@@ -36,4 +36,33 @@ class BankAccount:
     def show_balance(self):
         print(f"{self.owner} has ${self.__balance}")
 Andy = BankAccount("Andy", 150)
+print(Andy.__dict__) """
+
+""" class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+    
+Andy = Hero("Andy", 100, ["Tree Branch"])
 print(Andy.__dict__)
+
+Andy.buy("Sword - atk: 34 dmg")
+print(Andy.__dict__) """
+
+class Pet:
+    def __init__(self, name, happiness_level):
+        self.name = name
+        self.__happiness_level = happiness_level
+    
+    def play(self, amount):
+        Game = input("What game is your pet playing?: ")
+        print(f"{self.name} is playing {Game}"!)
+        self.__happiness_level +=10
+
+
+    
