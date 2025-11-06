10d34c2fca1c753f231346e6a2d3c80c0e7bfb3e
andychen313 <andyc313@nycstudents.net>
2025-11-06 09:26:03 -0500

    Update pet.py

--- pet.py
+++ pet.py
@@ -54,16 +54,43 @@ print(Andy.__dict__)
 Andy.buy("Sword - atk: 34 dmg")
 print(Andy.__dict__) """
 
-class Pet:
+""" class Pet:
     def __init__(self, name, happiness_level):
         self.name = name
         self.__happiness_level = happiness_level
     
     def play(self, amount: int):
         self.__happiness_level = 0
+        amount = 10
+        self.__happiness_level +=amount
         print(f"Sean is playing and gained {amount} happiness.")
 Sean = Pet("Sean", 0)
-print(Sean.__dict__)
+print(Sean.__dict__) """
+
+""" class Pet:
+    def __init__(self, name, happinesslevel):
+        self.name = name
+        self.__happinesslevel__ = happinesslevel
+
+    def play(self, game, amountgain):
+        self.__happinesslevel__ +=amountgain
+        print(f"Sean played {game} and now has {self.__happinesslevel__} happiness.")
 
 
-    
+Sean = Pet("Sean", 0)
+Sean.play("with a frisbee", 10) """
+
+class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.__money__ = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+Jillian = Hero("Jillian", 150, ["Potion"])
+print(Jillian.__dict__)
+
+Jillian.buy({"title": "Sword", "atk": 34})
+print(Jillian.__dict__)
