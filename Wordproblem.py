f531cda3ab406e1e83845f259a344fb3bec2221d
andychen313 <andyc313@nycstudents.net>
2025-11-19 09:25:29 -0500

    Update Wordproblem.py

--- Wordproblem.py
+++ Wordproblem.py
@@ -9,22 +9,22 @@ def portal_login_check(email, password):
     password = password.strip()
     if len(password) < 8:
         return "Error! Invalid Password! Password must include at least 8 characters!"
-    found_digit = False
+    digit_found = False
     for char in password:
         if char.isdigit():
-            found_digit = True
+            digit_found = True
             break
-    if not found_digit:
+    if not digit_found:
         return "Error! Invalid Password! Password must include at least one digit!"
-    found_upper = False
+    upper_found = False
     for char in password:
         if char.isupper():
-            found_upper = True
+            upper_found = True
             break
-    if not found_upper:
+    if not upper_found:
         return "Error! Invalid Password! Password must include at least one uppercase letter!"
     return {
         "email": email,
         "password": password
     }
-print(portal_login_check("emailtest123@gmail.com", "BestPass123"))
+print(portal_login_check("testemail123@gmail.com", "Nopass18"))
