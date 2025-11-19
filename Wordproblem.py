351adc3d259a387e742b26f93a6ca8acac06fc0c
andychen313 <andyc313@nycstudents.net>
2025-11-18 20:02:34 -0500

    Update Wordproblem.py

--- Wordproblem.py
+++ Wordproblem.py
@@ -25,10 +25,39 @@ portal_login("emailtest123@gmail.com", "Lkf001@") """
     if len(password)
 portal_login_check("emailtest123gmail.com") """
     
-def portal_login_check(password):
+""" def portal_login_check(password):
     password.strip(" ")
     if len(password) <= 8:
         print("Error! Invalid Password! Password must include at least 8 characters!")
     elif 
     else:
-        print("Valid Email!")
+        print("Valid Email!") """
+
+def portal_login_check(email, password):
+    if type(email) != str:
+        return "Error! Invalid Email! Email must be a string!"
+    if "@" not in email:
+        return "Error! Invalid Email! Email must include '@'"
+    if type(password) != str:
+        return "Error! Invalid Password! Password must be a string!"
+    if len(password) < 8:
+        return "Error! Invalid Password! Password must include at least 8 characters!"
+    found_digit = False
+    for char in password:
+        if char.isdigit():
+            found_digit = True
+            break
+    if not found_digit:
+        return "Error! Invalid Password! Password must include at least one digit!"
+    found_upper = False
+    for char in password:
+        if char.isupper():
+            found_upper = True
+            break
+    if not found_upper:
+        return "Error! Invalid Password! Password must include at least one uppercase letter!"
+    return {
+        "email": email,
+        "password": password
+    }
+print(portal_login_check("emailtest123@gmail.com", "BestPass123"))
